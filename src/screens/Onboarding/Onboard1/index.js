517a14d7b056a26dec80ed1e6d1d4a00e2df0a0b
olatunjiemanuel <olatunjiemanuel15@gmail.com>
2023-07-06 20:56:29 +0100

    touchable

--- src/screens/Onboarding/Onboard1/index.js
+++ src/screens/Onboarding/Onboard1/index.js
@@ -1,20 +1,27 @@
 import { StyleSheet, Text, View, Button } from "react-native";
 import React from "react";
+import { TouchableOpacity } from "react-native";
 
 const Onboard1 = ({ navigation }) => {
   return (
     <View>
       <Text>Onboard1</Text>
-      <Button
-        title="Go to onboard 2"
+      <TouchableOpacity
         onPress={() => {
           navigation.navigate("Onboard2");
         }}
-      />
+        style={styles.buttonCntnr}
+      >
+        <Text>title="Go to onboard 2"</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default Onboard1;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  buttonCntnr: {
+    backgroundColor: "blue",
+  },
+});
