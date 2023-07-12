e27ef6c141c8d3d31248f00bdfc78edd5d1dda1f
olatunjiemanuel <olatunjiemanuel15@gmail.com>
2023-07-12 20:15:29 +0100

    forms components

--- src/components/FormComponent/index.js
+++ src/components/FormComponent/index.js
@@ -1,10 +1,19 @@
 import { StyleSheet, Text, View, TextInput } from "react-native";
 import React from "react";
 
-const FormComponent = () => {
+const FormComponent = (props) => {
+  const { formName, placeHolder, onFocus, borderColor, color } = props;
+
   return (
-    <View>
-      <TextInput style={styles.textInput} />
+    <View style={[styles.formCntnr, { borderColor: borderColor }]}>
+      <View>
+        <Text style={[{ color: color }]}>{formName}</Text>
+      </View>
+      <TextInput
+        style={styles.textInput}
+        placeholder={placeHolder}
+        onFocus={onFocus}
+      />
     </View>
   );
 };
@@ -12,7 +21,12 @@ const FormComponent = () => {
 export default FormComponent;
 
 const styles = StyleSheet.create({
-  textInput: {
+  formCntnr: {
     borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+  textInput: {
+    height: 30,
   },
 });
--- src/screens/Onboarding/Onboard2/index.js
+++ src/screens/Onboarding/Onboard2/index.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "react-native";
 
 //screen imports
@@ -8,8 +8,12 @@ import AppStack from "../../AppStack";
 // component imports
 import FormComponent from "../../../components/FormComponent";
 import PageHeader from "../../../components/PageHeader";
+import ButtonComponent from "../../../components/ButtonComponent";
 
 const Onboard2 = ({ navigation, onComplete }) => {
+  const [isEmailFocused, setEmailFocused] = useState(false);
+  const [isPassFocused, setPassFocused] = useState(false);
+
   const handleOnboardingComplete = () => {
     onComplete();
   };
@@ -24,9 +28,45 @@ const Onboard2 = ({ navigation, onComplete }) => {
           TextColour="#008000"
         />
       </View>
-
-      <View>
-        <FormComponent />
+      <View style={styles.emailCntnr}>
+        <FormComponent
+          formName="Email"
+          placeHolder="Enter your email"
+          onFocus={() => {
+            setPassFocused(false);
+            setEmailFocused(true);
+          }}
+          borderColor={isEmailFocused ? "#008000" : null}
+          color={isEmailFocused ? "#008000" : null}
+        />
+      </View>
+      <View style={styles.passCntnr}>
+        <FormComponent
+          formName="Password"
+          placeHolder="Create a password"
+          onFocus={() => {
+            setEmailFocused(false);
+            setPassFocused(true);
+          }}
+          borderColor={isPassFocused ? "#008000" : null}
+          color={isPassFocused ? "#008000" : null}
+        />
+      </View>
+      <View style={styles.RegisterbuttonCntnr}>
+        <ButtonComponent
+          bgColour="#008000"
+          onPress
+          ButtonText="Create account"
+          textColour="#fff"
+        />
+      </View>
+      <View style={styles.SignInbuttonCntnr}>
+        <ButtonComponent
+          bgColour="#008000"
+          onPress
+          ButtonText="Sign In"
+          textColour="#fff"
+        />
       </View>
       <Button title="Got to AppStack" onPress={handleOnboardingComplete} />
     </View>
@@ -41,4 +81,8 @@ const styles = StyleSheet.create({
     paddingTop: Platform.OS === "android" ? 30 : 50,
     paddingHorizontal: 20,
   },
+  emailCntnr: { marginTop: 40 },
+  passCntnr: { marginTop: 20 },
+  RegisterbuttonCntnr: { marginTop: 150 },
+  SignInbuttonCntnr: { marginTop: 20 },
 });
